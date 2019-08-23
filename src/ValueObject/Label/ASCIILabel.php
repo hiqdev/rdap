532d20385e1d89289566870bb1f6c23ed9c41052
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2019-08-23 19:40:00 +0300

    Simplified ASCIILabel class

--- src/ValueObject/Label/ASCIILabel.php
+++ src/ValueObject/Label/ASCIILabel.php
@@ -2,46 +2,25 @@
 
 namespace hiqdev\rdap\core\ValueObject\Label;
 
-use http\Exception\InvalidArgumentException;
+use InvalidArgumentException;
 
 abstract class ASCIILabel extends Label
 {
-    /**
-     * @var string[]
-     */
-    public static $bytes;
-
     public function __construct(string $label)
     {
         parent::__construct($label);
-        self::$bytes = $this->getBytes();
+
         if (!self::checkContains($label)) {
             throw new InvalidArgumentException('wrong character in label string');
         }
     }
 
     /**
-     * @return string
-     */
-    private function getBytes(): string
-    {
-        $res = '';
-        foreach (range(32, 127) as $elem) {
-            $res .= chr($elem);
-        }
-        return $res;
-    }
-
-    /**
+     * @param string $string
      * @return bool
      */
-    public static function checkContains($string): bool
+    public static function checkContains(string $string): bool
     {
-        foreach (str_split($string) as $char) {
-            if (!strpos(self::$bytes, $char)) {
-                return false;
-            }
-        }
-        return true;
+        return mb_check_encoding($string, 'ASCII');
     }
 }
