d9f491dba6c4bb6760ed4c7e0ac2af68d0e2560d
strorch <smy980807@ukr.net>
2019-08-27 17:55:27 +0300

    search result implemented

--- src/ValueObject/IpV6Address.php
+++ src/ValueObject/IpV6Address.php
@@ -4,6 +4,9 @@ namespace hiqdev\rdap\core\ValueObject;
 
 final class IpV6Address implements IpAddress
 {
+    /**
+     * @inheritDoc
+     */
     public function getHostAddress(): string
     {
         // TODO: Implement getHostAddress() method.
