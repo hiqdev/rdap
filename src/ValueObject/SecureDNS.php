de5efcfb6c9f9d628128807de58139b12f6956b5
strorch <smy980807@ukr.net>
2019-08-27 17:22:07 +0300

    added phpdoc and implemented classes

--- src/ValueObject/SecureDNS.php
+++ src/ValueObject/SecureDNS.php
@@ -4,10 +4,95 @@
 namespace hiqdev\rdap\core\ValueObject;
 
 
+use hiqdev\rdap\core\ValueObject\SecureDNS\DSData;
+use hiqdev\rdap\core\ValueObject\SecureDNS\KeyData;
+
 class SecureDNS
 {
-    public function __construct()
-    {
+    /**
+     * @var bool
+     */
+    private $zoneSigned;
 
+    /**
+     * @var bool
+     */
+    private $delegationSigned;
+
+    /**
+     * @var int
+     */
+    private $maxSigLife;
+
+    /**
+     * @var DSData[]
+     */
+    private $dsData;
+
+    /**
+     * @var KeyData[]
+     */
+    private $keyData;
+
+    /**
+     * SecureDNS constructor.
+     * @param bool $zoneSigned
+     * @param bool $delegationSigned
+     * @param int $maxSigLife
+     * @param DSData[] $dsData
+     * @param KeyData[] $keyData
+     */
+    public function __construct(
+        bool $zoneSigned,
+        bool $delegationSigned,
+        int $maxSigLife,
+        array $dsData,
+        array $keyData
+    ) {
+        $this->delegationSigned = $delegationSigned;
+        $this->zoneSigned = $zoneSigned;
+        $this->maxSigLife = $maxSigLife;
+        $this->dsData = $dsData;
+        $this->keyData = $keyData;
+    }
+
+    /**
+     * @return DSData[]
+     */
+    public function getDsData(): array
+    {
+        return $this->dsData;
+    }
+
+    /**
+     * @return KeyData[]
+     */
+    public function getKeyData(): array
+    {
+        return $this->keyData;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxSigLife(): int
+    {
+        return $this->maxSigLife;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDelegationSigned(): bool
+    {
+        return $this->delegationSigned;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isZoneSigned(): bool
+    {
+        return $this->zoneSigned;
     }
 }
--- src/ValueObject/SecureDNS/KeyData.php
+++ src/ValueObject/SecureDNS/KeyData.php
@@ -4,10 +4,87 @@
 namespace hiqdev\rdap\core\ValueObject\SecureDNS;
 
 
-class KeyData
-{
-    public function __construct()
-    {
+use hiqdev\rdap\core\Entity\Link;
+use hiqdev\rdap\core\ValueObject\Event;
 
+/**
+ * Class KeyData object, that can be one of the following members:
+ *  +   flags -- an integer representing the flags field value in
+ *  the DNSKEY record [RFC4034] in presentation format
+ *  +   protocol -- an integer representation of the protocol field
+ *  value of the DNSKEY record [RFC4034] in presentation format
+ *  +   publicKey -- a string representation of the public key in
+ *  the DNSKEY record [RFC4034] in presentation format
+ *  +   algorithm -- an integer as specified by the algorithm field
+ *  of a DNSKEY record as specified by [RFC4034] in presentation
+ *  format
+ *  +   events
+ *  +   links
+ * @package hiqdev\rdap\core\ValueObject\SecureDNS
+ */
+
+class KeyData extends AbstractData
+{
+    /**
+     * @var string
+     */
+    private $flags;
+
+    /**
+     * @var string
+     */
+    private $protocol;
+
+    /**
+     * @var string
+     */
+    private $publicKey;
+
+    /**
+     * KeyData constructor.
+     * @param Event[] $events
+     * @param Link[] $links
+     * @param int $algorythm
+     * @param string $flags
+     * @param string $protocol
+     * @param string $publicKey
+     */
+    public function __construct(
+        array $events,
+        array $links,
+        int $algorythm,
+        string $flags,
+        string $protocol,
+        string $publicKey
+    ) {
+        parent::__construct($events, $links, $algorythm);
+
+        $this->flags = $flags;
+        $this->publicKey = $publicKey;
+        $this->protocol = $protocol;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicKey(): string
+    {
+        return $this->publicKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFlags(): string
+    {
+        return $this->flags;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProtocol(): string
+    {
+        return $this->protocol;
     }
 }
