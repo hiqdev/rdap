28f8abc6f441a291dd7261d20f22cb42efd21890
strorch <smy980807@ukr.net>
2019-08-27 12:47:23 +0300

    created AutNum fields with getters

--- src/Entity/AutNum.php
+++ src/Entity/AutNum.php
@@ -41,8 +41,62 @@ final class AutNum extends Common
      */
     private $country;
 
-    public function __construct()
-    {
+    public function __construct(
+        string $handle,
+        int $startAutnum,
+        int $endAutnum,
+        string $name,
+        string $type,
+        string $country
+    ) {
         parent::__construct(ObjectClassName::AUTNUM());
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getHandle(): ?string
+    {
+        return $this->handle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEndAutnum(): int
+    {
+        return $this->endAutnum;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStartAutnum(): int
+    {
+        return $this->startAutnum;
     }
 }
