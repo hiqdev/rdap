9b484b7bab2a5d2a090876f03d415030d30f06b8
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2019-08-29 20:13:36 +0300

    Fixed tests

--- tests/unit/Domain/Entity/CommonTest.php
+++ tests/unit/Domain/Entity/CommonTest.php
@@ -5,6 +5,7 @@ namespace hiqdev\rdap\core\tests\unit\Domain\Entity;
 
 
 use DateTimeImmutable;
+use hiqdev\rdap\core\Constant\EventAction;
 use hiqdev\rdap\core\Constant\ObjectClassName;
 use hiqdev\rdap\core\Constant\Status;
 use hiqdev\rdap\core\Entity\Common;
@@ -41,8 +42,8 @@ class CommonTest extends TestCase
 
     public function testEvents(): void
     {
-        $event1 = Event::occurred('action1', new DateTimeImmutable());
-        $event2 = Event::occurred('action2', new DateTimeImmutable());
+        $event1 = Event::occurred(EventAction::REGISTRATION(), new DateTimeImmutable());
+        $event2 = Event::occurred(EventAction::LAST_CHANGED(), new DateTimeImmutable());
         $common = $this->getMockForAbstractClass(Common::class, [ObjectClassName::ENTITY()]);
         $common->addEvent($event1);
         $common->addEvent($event2);
--- tests/unit/Domain/Entity/EntityTest.php
+++ tests/unit/Domain/Entity/EntityTest.php
@@ -5,6 +5,7 @@ namespace hiqdev\rdap\core\tests\unit\Domain\Entity;
 
 
 use DateTimeImmutable;
+use hiqdev\rdap\core\Constant\EventAction;
 use hiqdev\rdap\core\Constant\Role;
 use hiqdev\rdap\core\Entity\Entity;
 use hiqdev\rdap\core\ValueObject\Event;
@@ -74,8 +75,8 @@ class EntityTest extends TestCase
 
     public function testAsEventActor(): void
     {
-        $event1 = Event::occurred('action1', new DateTimeImmutable());
-        $event2 = Event::occurred('action2', new DateTimeImmutable());
+        $event1 = Event::occurred(EventAction::REGISTRATION(), new DateTimeImmutable());
+        $event2 = Event::occurred(EventAction::LAST_CHANGED(), new DateTimeImmutable());
         $entity = new Entity();
         $entity->addAsEventActor($event1);
         $entity->addAsEventActor($event2);
